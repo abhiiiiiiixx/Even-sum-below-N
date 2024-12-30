50b45126c96c3728e5a232c477f6d89e8d095e6c
Abhijeet kumar shah <129621254+abhiiiiiiixx@users.noreply.github.com>
2024-12-30 06:50:21 +0530

    Add files via upload

---
+++ Abhi.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Abhi {
+    public static void main(String[] args) {
+       
+
+           
+        int sum =0;
+                 int num = 8;
+                 for (int i = 0; i < 100; i++) {
+                 int c = 2 * i;
+                    
+                    if (c <= num) {
+                        
+                        sum += c;
+                        
+                    }
+                }
+                System.out.println(sum);
+
+            
+            }
+        }
+
+    
